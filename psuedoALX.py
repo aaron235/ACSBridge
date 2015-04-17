266f70889cd8247ef31c7a661a312f1924f9612c
aaron235 <qwertyman159@gmail.com>
2015-04-17 12:23:04 -0700

    Added threading for the HTTP server

--- psuedoALX.py
+++ psuedoALX.py
@@ -2,6 +2,7 @@
 
 ##  Used for runing the HTTP server and the file watcher simultaneously
 import threading
+import time
 
 ##  sends the request to the receiving server and parses the request from the sending server
 import urllib
@@ -11,14 +12,14 @@ import http.server
 import socketserver
 
 ##  watches the directory for changes, specifically new .log files (not yet implemented)
-# from watchdog.observers import Observer
-# from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 ##  Logging
 import logging
 
 ##  Functions for handling/parsing HTTP requests
-from psuedoALX_HTTP import Handler
+from psuedoALX_http import Handler
 
 ##  Configuration options
 from psuedoALX_global import globalVars
@@ -28,28 +29,41 @@ from psuedoALX_global import globalVars
 ##  MAIN  ##
 ############
 
+def httpServe():
+	##  set up a socketserver on the configured address running a Handler to handle GET/POST
+	server = socketserver.TCPServer( ( globalVars['EXTERNAL_SERVER'], globalVars['PORT_IN'] ), Handler )
+
+	print( "HTTP listener started on %s:%s" % server.server_address )
+	globalVars['LOG'].info( "HTTP listener started on %s:%s" % server.server_address )
+	##  continue serving until a keyboard interrupt is recieved
+	server.serve_forever()
+
+	#server.socket.close()
+
+
+def watchFiles():
+	return 0
 
 
 def main():
 	logging.basicConfig( filename=globalVars['LOG_PATH'], level=logging.DEBUG,
 		format='%(asctime)s\t%(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S' )
 	globalVars['LOG'] = logging.getLogger( 'main' )
-	
+
 	print( "Logging to '%s'..." % globalVars['LOG_PATH'] )
 
+	##  Set up the HTTP server to run in another thread
+	httpThread = threading.Thread( target=httpServe, daemon=True )
+	httpThread.start()
+
 	try:
-		##  set up a socketserver on the configured address running a Handler to handle GET/POST
-		server = socketserver.TCPServer( ( globalVars['EXTERNAL_SERVER'], globalVars['PORT_IN'] ), Handler )
-
-		print( "HTTP listener started on {server}:{port}".format( server=server.server_address[0], port=server.server_address[1] ) )
-		globalVars['LOG'].info( "HTTP listener started on {server}:{port}".format( server=server.server_address[0], port=server.server_address[1] ) )
-		##  continue serving until a keyboard interrupt is recieved
-		server.serve_forever()
-
+		while True:
+			time.sleep(1)
 	except (KeyboardInterrupt, SystemExit):
 		print( "Keyboard interrupt recieved. Shutting down server." )
 		globalVars['LOG'].info( "Keyboard interrupt recieved. Shutting down server." )
-		server.socket.close()
+		httpThread.join()
+
 
 if __name__ == '__main__':
-    main()
+	main()
