19c382e95db26f10b51ea99c42a907d031cf5437
aaron235 <qwertyman159@gmail.com>
2015-04-17 08:18:51 -0700

    Fixed some spacing

--- psuedoALX_csv.py
+++ psuedoALX_csv.py
@@ -10,7 +10,7 @@ def writeCSV( params, writeDir ):
 	and formats params' keys into one row of CSV, and the values into another row of
 	CSV. It then writes them to the directory, in a .csv file named with the date.
 	"""
-	csv = open( writeDir + time.strftime('%Y-%m-%d_%H:%M:%S') + ".csv", 'w' )
+	csv = open( writeDir + time.strftime( '%Y-%m-%d_%H:%M:%S' ) + ".csv", 'w' )
 	csv.write( ','.join( params.keys() ) )
 	csv.write( '\n' )
 	csv.write( ','.join( params.values() ) )
