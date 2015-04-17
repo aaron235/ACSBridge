f7a232e8160b8c2e733233e5912d9f79a82b4607
aaron235 <qwertyman159@gmail.com>
2015-04-17 09:44:19 -0700

    Neatened up and fixed logging

--- psuedoALX.py
+++ psuedoALX.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
+##  Used for runing the HTTP server and the file watcher simultaneously
+import threading
+
 ##  sends the request to the receiving server and parses the request from the sending server
 import urllib
+
 ##  listens to FSM server on incoming port
 import http.server
 import socketserver
+
 ##  watches the directory for changes, specifically new .log files (not yet implemented)
 # from watchdog.observers import Observer
 # from watchdog.events import FileSystemEventHandler
+
 ##  Logging
 import logging
 
@@ -25,15 +31,20 @@ from psuedoALX_config import config
 
 def main():
 	##  Set up logging
-	logging.basicConfig( filename=config['LOG_PATH'], level=logging.DEBUG, format='%(asctime)s\t%(levelname)s: %message', datefmt='%Y-%m-%d %H:%M:%S' )
+	logging.basicConfig( filename=config['LOG_PATH'], level=logging.DEBUG,
+		format='%(asctime)s\t%(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S' )
 	print( "Logging to '%s'..." % config['LOG_PATH'] )
 
 	try:
+		##  set up a socketserver on the configured address running a Handler to handle GET/POST
 		server = socketserver.TCPServer( ( config['EXTERNAL_SERVER'], config['PORT_IN'] ), Handler )
-		print( "HTTP listener started on " + str( server.server_address ) )
-		logging.info( "HTTP listener started on " + str( server.server_address ) )
+
+		print( "HTTP listener started on {server}:{port}".format( server=server.server_address[0], port=server.server_address[1] ) )
+		logging.info( "HTTP listener started on {server}:{port}".format( server=server.server_address[0], port=server.server_address[1] ) )
+		##  continue serving until a keyboard interrupt is recieved
 		server.serve_forever()
-	except KeyboardInterrupt:
+
+	except (KeyboardInterrupt, SystemExit):
 		print( "Keyboard interrupt recieved. Shutting down server." )
 		logging.info( "Keyboard interrupt recieved. Shutting down server." )
 		server.socket.close()
