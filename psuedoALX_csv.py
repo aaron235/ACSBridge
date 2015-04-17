993e06859a0dbac94d9479045d35f2b334d3a999
aaron235 <qwertyman159@gmail.com>
2015-04-17 08:18:29 -0700

    Added documentation for a method

--- psuedoALX_csv.py
+++ psuedoALX_csv.py
@@ -5,6 +5,11 @@ import time
 import psuedoALX_config
 
 def writeCSV( params, writeDir ):
+	"""
+	Takes a dictionary 'params' and a directory to write to 'writeDir'
+	and formats params' keys into one row of CSV, and the values into another row of
+	CSV. It then writes them to the directory, in a .csv file named with the date.
+	"""
 	csv = open( writeDir + time.strftime('%Y-%m-%d_%H:%M:%S') + ".csv", 'w' )
 	csv.write( ','.join( params.keys() ) )
 	csv.write( '\n' )
